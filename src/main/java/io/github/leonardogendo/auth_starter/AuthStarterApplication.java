0e738ceb6d61e8539bfa3942ae099e344c0e2a34
Leonard Ogendo <ogendoleonard5@gmail.com>
2026-09-04 16:21:36 +0300

    chore(init): initial setup

---
+++ src/main/java/io/github/leonardogendo/auth_starter/AuthStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.leonardogendo.auth_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthStarterApplication.class, args);
+	}
+
+}
